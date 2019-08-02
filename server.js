9262855614d4ded3ef53762317ea919858357466
Ppraphan <praphan.eng@gmail.com>
2019-08-02 14:46:28 +0700

    02/08/2019 14:46

---
+++ server.js
@@ -0,0 +1,31 @@
+//npm modules
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  limit: '150mb',
+  extended: false ,
+}));
+
+
+//---- set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.get('/profile', function(req, res) {
+  res.render('pages/profile');
+});
+
+app.listen(8080);
+console.log('8080 is Running...');
